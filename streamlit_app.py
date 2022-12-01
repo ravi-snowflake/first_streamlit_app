ad74ef7816c5eed13149a435217195351feb2afa
ravi-snowflake <119571498+ravi-snowflake@users.noreply.github.com>
2022-12-01 13:47:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('My Parents new healthy dinner')
 streamlit.header('Breakfast Menu')
-Streamlit.text('Omega3 and Blueberry Oatmeal')
-Streamlit.text('Gobhi ka aanchar')
-Streamlit.text('Kauwa biryani')
+streamlit.text('Omega3 and Blueberry Oatmeal')
+streamlit.text('Gobhi ka aanchar')
+streamlit.text('Kauwa biryani')
