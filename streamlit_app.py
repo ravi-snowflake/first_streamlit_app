5ad6e8c14cf96bb2d3e79271c311aef5bf416b87
ravi-snowflake <119571498+ravi-snowflake@users.noreply.github.com>
2022-12-03 22:20:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,3 +46,5 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add','jackfrui
 streamlit.write('Thanks for adding ', add_my_fruit)
 add_my_fruit_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
 
+my_cur.execute("insert into fruit_load_list values ('test')")
+
