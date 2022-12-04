988bc7b292a506abf7048cc51dd9d111e1755409
ravi-snowflake <119571498+ravi-snowflake@users.noreply.github.com>
2022-12-04 11:16:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,16 +42,24 @@ except URLError as e:
 
 streamlit.stop()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('From Streamlit')")
+    return "Thanks for adding ", new_fruit
+  
+add_my_fruit = streamlit.text_input('What fruit would you like to add') 
+if streamlit.button('Add a fruit to the list'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
 my_cur.execute("SELECT * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
-streamlit.text("The fruit load list contains:")
+
 streamlit.dataframe(my_data_rows)
 
-add_my_fruit = streamlit.text_input('What fruit would you like to add','jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+
+
 add_my_fruit_response = requests.get("https://fruityvice.com/api/fruit/" + add_my_fruit)
 
-my_cur.execute("insert into fruit_load_list values ('From Streamlit')")
+
 
